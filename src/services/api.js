af1bcca1b76c0f2b9f295a6aa7c0398f154e178d
joão josé <joaojosefaleirosantos@gmail.com>
2023-08-09 19:34:47 -0300

    change baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,6 @@ import axios from 'axios'
 //Já exportando diretamente
 export const api = axios.create({
   //Literalmente a base da URL para acessar nosso backend-servidor
-  //URL do baseURL (tem que ser maiúscula)  
-  baseURL: 'http://localhost:3333'
+  //URL do baseURL (tem que ser maiúscula)
+  baseURL: 'https://bankin-api.onrender.com'
 })
